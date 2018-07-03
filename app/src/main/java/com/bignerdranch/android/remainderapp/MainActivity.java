7ef8fdab570cbef04701bec029be8aab7d549c6d
Nandu-D <nandud999@gmail.com>
2018-07-03 15:01:23 +0530

    Gave title to toolbar

--- app/src/main/java/com/bignerdranch/android/remainderapp/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/remainderapp/MainActivity.java
@@ -5,18 +5,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView mRecyclerView;
     private FloatingActionButton mFloatingActionButton;
     private RecyclerView.Adapter mRemainderAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mToolbar = findViewById(R.id.toolbar);
         mRecyclerView = findViewById(R.id.recycler_view);
         mFloatingActionButton = findViewById(R.id.floatingActionButton);
 
@@ -25,5 +28,7 @@ public class MainActivity extends AppCompatActivity {
 
         mRecyclerView.setLayoutManager(mLayoutManager);
         mRecyclerView.setAdapter(mRemainderAdapter);
+
+        mToolbar.setTitle("Remainder App");
     }
 }
